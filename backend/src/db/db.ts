3002fd24b87eb045efdc2b08485e69246945c704
AnishKhamkar7 <anishkhamkar7@gmail.com>
2024-09-20 17:14:43 +0530

    [Backend] feat: added MongoDB connection setup

---
+++ backend/src/db/db.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URL}`);
+
+    console.log("MONGO DB CONNECTED!!!");
+  } catch (error) {
+    console.log("MONGO CONNECTION FAILED!!!", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
