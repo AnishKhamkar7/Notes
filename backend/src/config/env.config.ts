d75173c93eece77e412223c750b0a0dbac7b2ff7
AnishKhamkar7 <anishkhamkar7@gmail.com>
2024-09-26 22:53:48 +0530

    [Backend] refactor: reconfiguration in env.config

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { config } from "dotenv";
 import { expand } from "dotenv-expand";
+import { error } from "console";
 
 expand(config());
 
@@ -21,7 +22,7 @@ const schema = z.object({
   //   JWT_REFRESH_TOKEN_SECRET: z.string().trim().min(1),
   //   JWT_REFRESH_TOKEN_EXPIRY: z.string().trim().min(1),
 
-  CLIENT_URL: z.string().trim().min(1),
+  // CLIENT_URL: z.string().trim().min(1),
 });
 
 const parsedEnv = schema.safeParse(process.env);
@@ -29,7 +30,7 @@ const parsedEnv = schema.safeParse(process.env);
 if (!parsedEnv.success) {
   console.error(
     "❌ Invalid environment variables:",
-    JSON.stringify(parsedEnv.error.format(), null, 4)
+    JSON.stringify(parsedEnv.error.format(), error)
   );
   process.exit(1);
 }
