c49967f0dea0403eb9e42b3f04f8f98e414c8d48
AnishKhamkar7 <anishkhamkar7@gmail.com>
2024-09-30 11:26:37 +0530

    [Frontend] refactor: added dynamic profile Info insertion

--- frontend/notes-app/src/components/Cards/ProfileInfo.tsx
+++ frontend/notes-app/src/components/Cards/ProfileInfo.tsx
@@ -1,13 +1,13 @@
 import { getInitials } from "../../utils/helper";
 
-function ProfileInfo({ onLogout }: { onLogout: any }) {
+function ProfileInfo({ onLogout, userInfo }: { onLogout: any; userInfo: any }) {
   return (
     <div className="flex items-center gap-3">
       <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
-        {getInitials("William Vangence")}
+        {getInitials(userInfo?.name)}
       </div>
       <div>
-        <p className="text-sm font-medium">William</p>
+        <p className="text-sm font-medium">{userInfo?.name}</p>
         <button className="text-sm tex-slate-700 underline" onClick={onLogout}>
           Logout
         </button>
